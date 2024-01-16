0322bfb3a74d7a01b90c6c8df2900b38299f7f22
ondrej_dobias <ondrej.dobias233@gmail.com>
2024-01-16 11:27:40 +0100

    matrix project_admin_site

--- matrix/projectApp/admin.py
+++ matrix/projectApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import datainsert
+from .models import post
+
+admin.site.register(datainsert)
+admin.site.register(post)
--- matrix/projectApp/models.py
+++ matrix/projectApp/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 
 # Create your models here.
-class MatrixDat(models.Model):
-    name = models.CharField
-    color = models.CharField
+class datainsert(models.Model):
+        title = models.CharField(max_length=200, default="x")
+        name = models.CharField(max_length=200, default="x")
+        play = models.CharField(max_length=200, default="x")
+        play2 = models.CharField(max_length=200, default="x")
+class post(models.Model):
+    author = models.CharField(max_length=200, default="x")
+    title = models.CharField(max_length=200, default="x")
+    todaysmood = models.CharField(max_length=200, default="x")
+    pocet = models.DecimalField(max_digits=10, decimal_places=2)
+
